d48fc05cba758bac87e7c75031e639ed123c3004
Nihar Darji <nihardarji@Nihars-MBP.cable.rcn.com>
2021-01-23 17:08:01 -0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_END} mode on port ${PORT}`)
+)
