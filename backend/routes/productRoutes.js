a6a6724e94c59b06f1dde6099b0537c25eb4c993
Nihar Darji <nihardarji@Nihars-MBP.cable.rcn.com>
2021-01-31 02:11:08 -0600

    added product review functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProductById, getProducts, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
+import { getProductById, getProducts, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
@@ -11,5 +11,7 @@ router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
     .put(protect, admin, updateProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
