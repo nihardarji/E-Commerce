7ff91f83cd798d2b2f0333239486cdb26dfc82fd
Nihar Darji <nihardarji@Nihars-MBP.cable.rcn.com>
2021-01-31 22:27:10 -0600

    -Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,13 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 // __dirname is not supported in ES module
 const __dirname = path.resolve()
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
 
 app.use(notFound)
 app.use(errorHandler)
